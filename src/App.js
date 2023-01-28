8b0564feeab6d2113f42308f9f52bbedd4f6ffb3
Jackson Stone <20364733+jacksors@users.noreply.github.com>
2023-01-28 12:37:23 -0600

    update utils location

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './Navigation';
-import Home from './Home';
-import News from './News';
-import { auth, db } from '../utils/firebase';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./Navigation";
+import Home from "./Home";
+import News from "./News";
+import { auth, db } from "./utils/firebase";
 
-import BuySell from './BuySell';
+import BuySell from "./BuySell";
 
 function App() {
-	const [stock, setStock] = useState('');
-	const [stockHistory, setStockHistory] = useState([]);
-	const [stockLive, setStockLive] = useState({});
+  const [stock, setStock] = useState("");
+  const [stockHistory, setStockHistory] = useState([]);
+  const [stockLive, setStockLive] = useState({});
 
-	useEffect(() => {
-		console.log(stockHistory);
-	}, [stockHistory]);
+  useEffect(() => {
+    console.log(stockHistory);
+  }, [stockHistory]);
 
-	return (
-		<Router>
-			<div className="App">
-				<Navigation />
-				<header className="App-header" style={{ paddingTop: '70px' }}>
-					<Routes>
-						<Route
-							path="/buysell"
-							element={
-								<BuySell
-									stock={stock}
-									setStockHistory={setStockHistory}
-									stockHistory={stockHistory}
-									setStock={setStock}
-									stockLive={stockLive}
-									setStockLive={setStockLive}
-								/>
-							}
-						/>
-						<Route path="/" element={<Home />} />
-						<Route path="news" element={<News />} />
-					</Routes>
-				</header>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <header className="App-header" style={{ paddingTop: "70px" }}>
+          <Routes>
+            <Route
+              path="/buysell"
+              element={
+                <BuySell
+                  stock={stock}
+                  setStockHistory={setStockHistory}
+                  stockHistory={stockHistory}
+                  setStock={setStock}
+                  stockLive={stockLive}
+                  setStockLive={setStockLive}
+                />
+              }
+            />
+            <Route path="/" element={<Home />} />
+            <Route path="news" element={<News />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
